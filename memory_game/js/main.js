2462bcef535c0af0ddbc4193ec1119a9add554d6
Ben Miller <bmillsmc@Bens-MBP.home>
2019-09-24 16:01:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamodns",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 const cardsInPlay = [];
 function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,11 +30,13 @@ function checkForMatch () {
 	}
 }
 function flipCard (cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
-	console.log("User flipped "+ cards[cardId]);
+	console.log("User flipped "+ cards[cardId].rank)
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(0);
